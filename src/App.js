ca81446aa0cf5d2dfbbc266931ea77d7086d670d
Alexei <demidenko8888@gmail.com>
2021-03-03 11:01:29 +0300

    feat: add addRandomTileNumber

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import cloneDeep from 'lodash.clonedeep'
+import { useEffect, useState } from 'react'
 import { Board } from './components/Board/Board'
 
 function App() {
@@ -8,6 +9,41 @@ function App() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ])
+
+  const initialize = () => {
+    let newGrid = cloneDeep(data)
+    console.log('New greed: ', newGrid)
+
+    addRandomTileNumber(newGrid)
+    console.table(newGrid)
+    addRandomTileNumber(newGrid)
+    console.table(newGrid)
+    setData(newGrid)
+  }
+
+  const addRandomTileNumber = (newGrid) => {
+    let added = false
+    let gridFull = false
+    let attempts = 0
+    while (!added) {
+      if (gridFull) {
+        break
+      }
+
+      let rand1 = Math.floor(Math.random() * 4)
+      let rand2 = Math.floor(Math.random() * 4)
+      attempts++
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4
+        added = true
+      }
+    }
+  }
+
+  useEffect(() => {
+    initialize()
+  }, [])
+
   return <Board data={data} />
 }
 
