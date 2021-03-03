680bf1a5bf59eb1da682c3274ffebf7f0a5fd0c1
Alexei <demidenko8888@gmail.com>
2021-03-03 11:45:22 +0300

    feat: add moveLeft, moveRight, moveDown, moveUp functions

--- src/App.js
+++ src/App.js
@@ -40,6 +40,187 @@ function App() {
     }
   }
 
+  const moveLeft = (isArrNew) => {
+    console.log('move left')
+    let oldGrid = data
+    let newArray = cloneDeep(data)
+
+    for (let i = 0; i < 4; i++) {
+      let b = newArray[i]
+      let slow = 0
+      let fast = 1
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1
+          slow++
+          continue
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast++
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast]
+          b[fast] = 0
+          fast++
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast++
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast]
+            b[fast] = 0
+            fast = slow + 1
+            slow++
+          } else {
+            slow++
+            fast = slow + 1
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      addRandomTileNumber(newArray)
+    }
+    if (isArrNew) {
+      return newArray
+    } else {
+      setData(newArray)
+    }
+  }
+
+  const moveRight = (isArrNew) => {
+    console.log('move right')
+    let oldData = data
+    let newArray = cloneDeep(data)
+
+    for (let i = 3; i >= 0; i--) {
+      let b = newArray[i]
+      let slow = b.length - 1
+      let fast = slow - 1
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1
+          slow--
+          continue
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast--
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast]
+          b[fast] = 0
+          fast--
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast--
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast]
+            b[fast] = 0
+            fast = slow - 1
+            slow--
+          } else {
+            slow--
+            fast = slow - 1
+          }
+        }
+      }
+    }
+    if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
+      addRandomTileNumber(newArray)
+    }
+    if (isArrNew) {
+      return newArray
+    } else {
+      setData(newArray)
+    }
+  }
+
+  const moveDown = (isArrNew) => {
+    console.log('move down')
+    console.log(data)
+    let b = cloneDeep(data)
+    let oldData = JSON.parse(JSON.stringify(data))
+    for (let i = 3; i >= 0; i--) {
+      let slow = b.length - 1
+      let fast = slow - 1
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1
+          slow--
+          continue
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast--
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i]
+          b[fast][i] = 0
+          fast--
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast--
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i]
+            b[fast][i] = 0
+            fast = slow - 1
+            slow--
+          } else {
+            slow--
+            fast = slow - 1
+          }
+        }
+      }
+    }
+    if (JSON.stringify(b) !== JSON.stringify(oldData)) {
+      addRandomTileNumber(b)
+    }
+    if (isArrNew) {
+      return b
+    } else {
+      setData(b)
+    }
+  }
+
+  const moveUp = (isArrNew) => {
+    console.log('move up')
+    let b = cloneDeep(data)
+    let oldData = JSON.parse(JSON.stringify(data))
+    for (let i = 0; i < 4; i++) {
+      let slow = 0
+      let fast = 1
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1
+          slow++
+          continue
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast++
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i]
+          b[fast][i] = 0
+          fast++
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast++
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i]
+            b[fast][i] = 0
+            fast = slow + 1
+            slow++
+          } else {
+            slow++
+            fast = slow + 1
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+      addRandomTileNumber(b)
+    }
+    if (isArrNew) {
+      return b
+    } else {
+      setData(b)
+    }
+  }
+
   useEffect(() => {
     initialize()
   }, [])
