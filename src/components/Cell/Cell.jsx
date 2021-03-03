870ca1e2995a57586246463c1c0c7eba59a1488b
Alexei <demidenko8888@gmail.com>
2021-03-03 14:01:43 +0300

    feat: add a color-manager

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,5 +1,15 @@
+import { getColors } from '../../utils/colors-manager'
 import styles from './Cell.module.css'
 
 export const Cell = ({ num }) => {
-  return <div className={styles.cell}>{num}</div>
+  return (
+    <div
+      className={styles.cell}
+      style={{
+        background: getColors(num),
+        color: num === 2 || num === 4 ? '#645B52' : '#F7F4EF',
+      }}>
+      {num !== 0 ? num : ''}
+    </div>
+  )
 }
